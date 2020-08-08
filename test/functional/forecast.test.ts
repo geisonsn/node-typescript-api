ba49a8787f1d9145ca3f7305db364963965e575c
Geison Santos <geison.sn@gmail.com>
2020-08-08 00:40:42 +0000

    Add prettier to improving code readability

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,5 +1,5 @@
 describe('Before forecast functional tests', () => {
-  it('should return a forecast with just a few times', async() => {
+  it('should return a forecast with just a few times', async () => {
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
     expect(body).toEqual([
